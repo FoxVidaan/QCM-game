a9e04d2441234de33d3bd2f5cb764bbc5f83b6a5
FoxVidaan <andgel1512@hotmail.fr>
2021-10-14 12:15:56 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,244 @@
+let questions = [
+    {
+        'libelle': 'De quelle espèce est Raditz ?',
+        'reponses': {
+            'a': 'Namek',
+            'b': 'Humain',
+            'c': 'Saiyan',
+            'd': 'Cyborg',
+        },
+        'reponseValide': 'c'
+    }, {
+        'libelle': 'A qui Nail ressemble-t-il ?',
+        'reponses': {
+            'a': 'Piccolo',
+            'b': 'Krillin',
+            'c': 'Shenron',
+            'd': 'Dendé',
+        },
+        'reponseValide': 'a'
+    }, {
+        'libelle': 'Quel personnage tue Son Goku pour la deuxième fois ?',
+        'reponses': {
+            'a': 'Boo',
+            'b': 'Cell',
+            'c': 'Freezer',
+            'd': 'Shenron',
+        },
+        'reponseValide': 'b'
+    }, {
+        'libelle': 'De quelle planète Son Goku est-il originaire ?',
+        'reponses': {
+            'a': 'Allia',
+            'b': 'Terre',
+            'c': 'Namek',
+            'd': 'Vegeta',
+        },
+        'reponseValide': 'd'
+    }, {
+        'libelle': 'Quel personnage meurt le plus souvent ?',
+        'reponses': {
+            'a': 'Krillin',
+            'b': 'Yamcha',
+            'c': 'Chaozu',
+            'd': 'Tenshinhan',
+        },
+        'reponseValide': 'a'
+    }, {
+        'libelle': 'Combien y\'a t-il de dragon ball ?',
+        'reponses': {
+            'a': '2',
+            'b': '9',
+            'c': '7',
+            'd': '4',
+        },
+        'reponseValide': 'c'
+    },{
+        'libelle': 'Comment s\'appelle la femme de Vegeta',
+        'reponses': {
+            'a': 'Bulma',
+            'b': 'Videl',
+            'c': 'Chi-Chi',
+            'd': 'C-18',
+        },
+        'reponseValide': 'a'
+    },{
+        'libelle': 'Avec qui Goku va-t-il passer son entraînement chez Tortue Géniale ?',
+        'reponses': {
+            'a': 'Piccolo',
+            'b': 'Vegeta',
+            'c': 'Nappa',
+            'd': 'Krillin',
+        },
+        'reponseValide': 'd'
+    },{
+        'libelle': 'Qui entraîne le fils de Son Goku ?',
+        'reponses': {
+            'a': 'Yamcha',
+            'b': 'Tortue Géniale',
+            'c': 'Piccolo',
+            'd': 'Dendé',
+        },
+        'reponseValide': 'c'
+    },{
+        'libelle': 'Quel est le premier vœu réalisé par Shenron ?',
+        'reponses': {
+            'a': 'Une fourchette',
+            'b': 'Une culotte',
+            'c': 'Une assiette',
+            'd': 'Une montagne de nourriture',
+        },
+        'reponseValide': 'b'
+    },{
+        'libelle': 'Qui est le créateur des Dragon Balls ?',
+        'reponses': {
+            'a': 'Boo',
+            'b': 'Tout-Puissant',
+            'c': 'Shenron',
+            'd': 'Cell',
+        },
+        'reponseValide': 'b'
+    },{
+        'libelle': 'De quelle espèce est le Tout-Puissant ?',
+        'reponses': {
+            'a': 'Saiyan',
+            'b': 'Cyborg',
+            'c': 'Namek',
+            'd': 'Humain',
+        },
+        'reponseValide': 'c'
+    },{
+        'libelle': 'Qui est responsable de la destruction de la planète Végéta ?',
+        'reponses': {
+            'a': 'Freezer',
+            'b': 'Bibidi',
+            'c': 'Babidi',
+            'd': 'Boo',
+        },
+        'reponseValide': 'a'
+    },{
+        'libelle': 'Lequel de ces personnages fut le disciple de Tortue Géniale ?',
+        'reponses': {
+            'a': 'C-17',
+            'b': 'Bulma',
+            'c': 'Krillin',
+            'd': 'Cell',
+        },
+        'reponseValide': 'c'
+    },{
+        'libelle': 'Combien de fois Krilin meurt-il ?',
+        'reponses': {
+            'a': '1',
+            'b': '2',
+            'c': '3',
+            'd': '4',
+        },
+        'reponseValide': 'd'
+    },{
+        'libelle': 'Qui parvient à devenir le maire de Satan City ?',
+        'reponses': {
+            'a': 'Tortue Géniale',
+            'b': 'Hercule',
+            'c': 'Bulma',
+            'd': 'Son Gohan',
+        },
+        'reponseValide': 'b'
+    },{
+        'libelle': 'Quel personnage possède une queue de singe ?\n',
+        'reponses': {
+            'a': 'Dendé',
+            'b': 'Hercule',
+            'c': 'Son Goku',
+            'd': 'Yamcha',
+        },
+        'reponseValide': 'c'
+    },{
+        'libelle': 'Combien d\'épisode dure le plus long combat de l\'anime ?',
+        'reponses': {
+            'a': '10',
+            'b': '14',
+            'c': '19',
+            'd': '24',
+        },
+        'reponseValide': 'c'
+    },{
+        'libelle': 'Comment s\'appelle le frère de Son Goku ?',
+        'reponses': {
+            'a': 'Nappa',
+            'b': 'Vegeta',
+            'c': 'Son Goten',
+            'd': 'Raditz',
+        },
+        'reponseValide': 'd'
+    },{
+        'libelle': 'Sur quel ennemi Sangoku a-t-il lancé son premier Genkidama ?',
+        'reponses': {
+            'a': 'Raditz',
+            'b': 'Freezer',
+            'c': 'Cell',
+            'd': 'Boo',
+        },
+        'reponseValide': 'b'
+    },
+];
+let nbQuestions = 0;
+let score = 0;
+let reponsesBtn = document.querySelectorAll('.reponse');
+
+window.addEventListener('load', quizBuild);
+for (let button of reponsesBtn) {
+    button.addEventListener('click', () => {
+        if (nbQuestions < questions.length) {
+            if (button.id === questions[nbQuestions].reponseValide) {
+                score++;
+                console.log(score)
+            }
+        }
+        nbQuestions++;
+        quizBuild()
+    });
+}
+
+function quizBuild () {
+    if (nbQuestions < questions.length) {
+        let nbQuestionsSpan = document.querySelector('.nbQuestions');
+        nbQuestionsSpan.innerHTML = `${nbQuestions + 1} / ${questions.length}`;
+
+        let questionP = document.querySelector('.question');
+
+        let question = questions[nbQuestions];
+        questionP.innerHTML = question.libelle;
+
+
+        for (let i = 0; i < reponsesBtn.length; i++) {
+            reponsesBtn[i].innerHTML = question.reponses[reponsesBtn[i].id];
+        }
+    } else {
+        let app = document.querySelector('.app');
+
+        while (app.firstChild) {
+            app.removeChild(app.firstChild);
+        }
+
+        let scoreBoard = document.createElement('span');
+        let p = document.createElement('p');
+
+        scoreBoard.innerText = `${score} / ${questions.length}`;
+        p.innerText = jugement();
+
+        app.appendChild(scoreBoard);
+        app.appendChild(p);
+    }
+};
+
+function jugement() {
+    if (score < 5) {
+        return "Il va falloir sérieusement réviser vos classiques ^^'";
+    } else if (score >= 5 && score < 10) {
+        return "C'est pas cool de dormir devant LE manga !";
+    } else if (score >= 10 && score < 15) {
+        return "Pas mal, mais vous allez devoir réserver du temps pour un revisionnage !";
+    } else {
+        return "Vous êtes une personne parfaitement respectable !";
+    }
+}
